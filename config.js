d55774ce85604e2d020cdd96f1d7133d390f4ab7
intelwizard120 <intelwizard120@gmail.com>
2022-08-05 17:11:23 -0400

    add configuration

---
+++ config.js
@@ -0,0 +1,13 @@
+const data = {
+    UniswapV2Router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',  // Uniswap V2 Router
+    SushiswapRouter: '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F',
+    MyAddress: '0xF481c92a7e5b113b094E093CF3454Ceb5F395B91',
+
+    // Mainnet Address
+    WETH: 'C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    DAI: '6b175474e89094c44da98b954eedeac495271d0f',
+    USDT: 'dac17f958d2ee523a2206206994597c13d831ec7',
+    USDC: 'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+}
+
+export default data;
